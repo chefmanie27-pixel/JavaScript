76a127c3bc89eba69cdd107b68b3f5870055d185
chefmanie27-pixel <chefmanie27@gmail.com>
2026-05-19 10:06:40 +0200

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,21 @@
+// TODO: Create a variable called firstName and store your first name in it
+// TODO: Create a variable called lastName and store your last name in it
+// TODO: Create a variable called age using let (you'll modify this later)
+// TODO: Create a fullName variable that combines firstName and lastName
+// TODO: Print a greeting to the console using these variables
+// TODO: Try changing the age variable to a different value
+
+
+const firstName = "Azhar";
+
+const lastName = "Manie";
+
+let age = 24;
+
+let fullName = firstName + " " + lastName;
+
+console.log("Hello I'm " + fullName + " and I'm " + age + " years old.");
+
+age = 25;
+
+console.log("This year, I'll be " + age + " years old.");
