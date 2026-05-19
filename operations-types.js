703983e00d7a0e8b3d799b889e9d4a8ed97d4cc4
chefmanie27-pixel <chefmanie27@gmail.com>
2026-05-19 11:27:36 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,16 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 10;
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 5;
+// TODO: Convert number1 to a string and store it in a new variable
+let number1String = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum
+let sum = number1 + number2;
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = number1 * number2;
+// TODO: Print all results to the console
+console.log("Number 1:", number1);
+console.log("Number 2:", number2);
+console.log("Number 1 as a string:", number1String);
+console.log("Sum of number1 and number 2:", sum);
+console.log("Product of number1 and number2:", product);
